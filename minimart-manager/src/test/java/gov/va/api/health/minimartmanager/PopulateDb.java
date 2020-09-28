e1f91d40f9bb5237d2106407db37a315713c0f5a
Adrian Garcia <adrian.garcia@libertyits.com>
2020-09-28 10:07:19 -0400

    Merged with other ETL branch for concentration of updates

--- minimart-manager/src/test/java/gov/va/api/health/minimartmanager/PopulateDb.java
+++ minimart-manager/src/test/java/gov/va/api/health/minimartmanager/PopulateDb.java
@@ -107,7 +107,7 @@ public class PopulateDb {
           resource,
           importDirectoryPath,
           configFilePath);
-      
+
       MitreMinimartMaker.sync(importDirectoryPath, resource, entityManagerFactory);
 
       latestResourceEtlStatusLoader.insertIntoEtlTable(resource);
