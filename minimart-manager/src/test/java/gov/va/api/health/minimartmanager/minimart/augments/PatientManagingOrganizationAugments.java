d46fc4dec07c6ba179dc43d03d08c6e396f18fb3
JJ Abando <jesse.abando@libertyits.com>
2020-10-13 11:56:34 -0400

    Refactored managingOrganization augment, organizations loaded as only a list of strings

--- minimart-manager/src/test/java/gov/va/api/health/minimartmanager/minimart/augments/PatientManagingOrganizationAugments.java
+++ minimart-manager/src/test/java/gov/va/api/health/minimartmanager/minimart/augments/PatientManagingOrganizationAugments.java
@@ -7,29 +7,26 @@ import gov.va.api.health.dataquery.service.controller.patient.DatamartPatient;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class PatientManagingOrganizationAugments {
 
-    private static final List<Optional<DatamartReference>> ORGANIZATION_REFERENCES =
+    private static final List<Optional<String>> ORGANIZATION_REFERENCES =
             loadOrganizations();
 
     static DatamartPatient addManagingOrganization(Augmentation.Context<DatamartPatient> ctx){
-        Optional<String> randOrg = ctx.random(ORGANIZATION_REFERENCES).flatMap(DatamartReference::reference);
-        return ctx.resource()
-                .managingOrganization(
-                        Optional.ofNullable(randOrg.isPresent() ? randOrg : null)
-                                .orElse(null));
+    return ctx.resource()
+        .managingOrganization(ctx.random(ORGANIZATION_REFERENCES));
     }
 
-  private static List<Optional<DatamartReference>> loadOrganizations() {
-    List<Optional<DatamartReference>> o =
+  private static List<Optional<String>> loadOrganizations() {
+    List<Optional<String>> o =
         ReferenceLoader.loadReferencesFor(
             DatamartOrganization.class,
             dm ->
                 DatamartReference.builder()
-                    .type(Optional.of("Organization"))
                     .reference(Optional.ofNullable(dm.cdwId()))
-                    .build());
+                    .build()).stream().map(d -> d.flatMap(DatamartReference::reference)).collect(Collectors.toList());
     o.add(Optional.empty());
     return o;
     }
