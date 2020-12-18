a86b4b3711be5f662d391719ff43db8e764598b6
jhulbert <joshua.hulbert@libertyits.com>
2020-12-18 10:10:40 -0500

    Dont add NPI

--- db/migration/V9_0__add_patient_columns_ssn_npi.sql
+++ db/migration/V9_0__add_patient_columns_ssn_npi.sql
@@ -2,11 +2,5 @@
 ALTER TABLE [app].[Patient] ADD [SSN] [varchar](50) NULL
 GO
 
-ALTER TABLE [app].[Patient] ADD [NPI] [varchar](50) NULL
-GO
-
 CREATE INDEX [IX_Patient_SSN] on [App].[Patient]([SSN])
-GO
-
-CREATE INDEX [IX_Patient_NPI] on [App].[Patient]([NPI])
 GO
