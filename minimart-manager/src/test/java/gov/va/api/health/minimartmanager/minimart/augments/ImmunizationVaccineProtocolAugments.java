fe0e22eec1401f1a6aa5f4a3fb0935e5cf8c53ff
jhulbert <joshua.hulbert@libertyits.com>
2021-01-19 11:50:44 -0500

    Rev DQ

--- minimart-manager/src/test/java/gov/va/api/health/minimartmanager/minimart/augments/ImmunizationVaccineProtocolAugments.java
+++ minimart-manager/src/test/java/gov/va/api/health/minimartmanager/minimart/augments/ImmunizationVaccineProtocolAugments.java
@@ -8,28 +8,32 @@ import java.util.Optional;
 
 public class ImmunizationVaccineProtocolAugments {
 
-    private static final List<Optional<DatamartImmunization.VaccinationProtocols>> VACC_PROTOCOLS =
-            List.of(
-                    vp("Series 1", 2),
-                    vp("Series 1", 1),
-                    vp("Booster", 1),
-                    Optional.empty());
+  private static final List<Optional<DatamartImmunization.VaccinationProtocols>> VACC_PROTOCOLS =
+      List.of(
+              vp("Series 1", 2),
+              vp("Series 1", 1),
+              vp("Booster", 1),
+              Optional.empty());
 
-    public static void main(String[] args) {
-        Augmentation.forResources(DatamartImmunization.class)
-                .whenMatching(Objects::nonNull)
-                .transform(ImmunizationVaccineProtocolAugments::addVaccineProtocol)
-                .build()
-                .rewriteFiles();
-    }
+  public static void main(String[] args) {
+    Augmentation.forResources(DatamartImmunization.class)
+        .whenMatching(Objects::nonNull)
+        .transform(ImmunizationVaccineProtocolAugments::addVaccineProtocol)
+        .build()
+        .rewriteFiles();
+  }
 
-    static DatamartImmunization addVaccineProtocol(Augmentation.Context<DatamartImmunization> ctx) {
-        ctx.resource().vaccinationProtocols(ctx.random(VACC_PROTOCOLS));
-        return ctx.resource();
-    }
+  static DatamartImmunization addVaccineProtocol(Augmentation.Context<DatamartImmunization> ctx) {
+    ctx.resource().vaccinationProtocols(ctx.random(VACC_PROTOCOLS));
+    return ctx.resource();
+  }
 
-    private static Optional<DatamartImmunization.VaccinationProtocols> vp(String series, Integer seriesDoses) {
-        return Optional.of(DatamartImmunization.VaccinationProtocols.builder().seriesDoses(seriesDoses).series(series).build());
-    }
-
+  private static Optional<DatamartImmunization.VaccinationProtocols> vp(
+      String series, Integer seriesDoses) {
+    return Optional.of(
+        DatamartImmunization.VaccinationProtocols.builder()
+            .seriesDoses(Optional.of(seriesDoses))
+            .series(Optional.of(series))
+            .build());
+  }
 }
