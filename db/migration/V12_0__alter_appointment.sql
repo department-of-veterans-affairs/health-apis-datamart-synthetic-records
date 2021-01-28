b67d8ec704e8a1803e52fc29c9a340149e8ccc03
Ian LaFlamme <ian.laflamme@libertyits.com>
2021-01-28 14:27:11 -0500

    .

--- db/migration/V12_0__alter_appointment.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE [App].[Appointment] ADD [LocationSID] [int] NULL
-ALTER TABLE [App].[Appointment] DROP CONSTRAINT DF__Appointme__LastU__6FE99F9F 
-ALTER TABLE [APP].[Appointment] ALTER COLUMN [LastUpdated] [date]
