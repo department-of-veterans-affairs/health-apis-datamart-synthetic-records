8e44974399c9a8b61fc84cedbf56b25fe770da62
Adrian Garcia Liberty ITS <adriangarcia@Adrians-MacBook-Pro.local>
2021-06-07 10:30:01 -0400

    Made PractitionerRole column not null, extended Specialty length to 255 characters, and removed sta3n column

--- db/migration/V17_0__create_practitioner_role.sql
+++ db/migration/V17_0__create_practitioner_role.sql
@@ -2,12 +2,11 @@ Create table [App].[PractitionerRole]
 (
   [CDWIdNumber] [int] not null,
   [CDWIdResourceCode] [char](1) not null,
-  [Sta3n] [smallint] not null,
-  [Specialty] [varchar](100) null,
+  [Specialty] [varchar](255) null,
   [PractitionerGivenName] [varchar](50) null,
   [PractitionerFamilyName] [varchar](50) null,
   [PractitionerNPI] [varchar](50) null,
   --JSON Payload
-  [PractitionerRole] [varchar](MAX),
+  [PractitionerRole] [varchar](MAX) not null,
   [LastUpdated] [date] not null
 )
